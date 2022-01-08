85141475a4bb232afda567e59f29b014e2494bd7
Uwe McVogt <mac@uv-software.de>
2022-01-08 10:15:00 +0100

    Update examples to run with version 0.10
    Relates to [MACCAN-49]

--- Examples/python/pcbusb_recv.py
+++ Examples/python/pcbusb_recv.py
@@ -6,43 +6,48 @@ import select                  ## Waiting for I/O completion
 import signal                  ## Set handlers for asynchronous events
 import sys                     ## System-specific parameters and functions
 
+
 def sigterm(signo, frame):
-    print #"got signal %d" % signo
-    PCANBasic().Uninitialize(PCAN_NONEBUS)
-    sys.exit(0)
-
-class PCBUSB_Recv:
-    pcan = PCANBasic()
-    channel = PCAN_USBBUS1
-    baudrate = PCAN_BAUD_250K
-    result = pcan.Initialize(channel,baudrate)
-    print "init result = ",result
+    result = PCANBasic().Uninitialize(PCAN_NONEBUS)
     if result != PCAN_ERROR_OK:
-        print "Error!",pcan.GetErrorText(result)
+        print("Error: {}".format(PCANBasic().GetErrorText(result)))
         sys.exit(1)
-
-    result = pcan.GetValue(channel,PCAN_RECEIVE_EVENT)
-    if result[0] == PCAN_ERROR_OK:
-        fd = result[1]
     else:
-        fd = -1
+        sys.exit(0)
 
-    signal.signal(signal.SIGTERM, sigterm)
-    signal.signal(signal.SIGINT, sigterm)
 
-    while 1:
-        result = pcan.Read(channel)
-        if result[0] == PCAN_ERROR_OK:
-            CANmsg=result[1]
-            print "ID %3X" % CANmsg.ID,
-            print "len=%d [ " % CANmsg.LEN,
-            for i in range(CANmsg.LEN):
-                print "%02X " % CANmsg.DATA[i],
-            print "]"
-        elif result[0] == PCAN_ERROR_QRCVEMPTY:
-            if fd != -1:
-                select.select([fd],[],[])
-            else:
-                time.sleep(0.001)
+pcan = PCANBasic()
+channel = PCAN_USBBUS1
+baudrate = PCAN_BAUD_250K
+result = pcan.Initialize(channel,baudrate)
+if result != PCAN_ERROR_OK:
+    print("Error: {}".format(pcan.GetErrorText(result)))
+    sys.exit(1)
 
-# end PCBUSB_Recv
+result = pcan.GetValue(channel,PCAN_RECEIVE_EVENT)
+if result[0] == PCAN_ERROR_OK:
+    fd = result[1]
+else:
+    fd = -1
+
+print("Press ^C to abort...")
+signal.signal(signal.SIGTERM, sigterm)
+signal.signal(signal.SIGINT, sigterm)
+
+while 1:
+    result = pcan.Read(channel)
+    if result[0] == PCAN_ERROR_OK:
+        CANmsg = result[1]
+        print("ID={:03X} LEN={:d} DATA=[{}]".format(CANmsg.ID,CANmsg.LEN,' '.join('{:02X}'.format(x) for x in CANmsg.DATA)))
+    elif result[0] == PCAN_ERROR_QRCVEMPTY:
+        if fd != -1:
+            select.select([fd],[],[])
+        else:
+            time.sleep(0.001)
+
+result = pcan.Uninitialize()
+if result != PCAN_ERROR_OK:
+    print("Error: {}".format(pcan.GetErrorText(result)))
+    sys.exit(1)
+else:
+    sys.exit(0)
