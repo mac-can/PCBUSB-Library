2735788a4898fadcf39e25bc4a1c4b35b5cdca26
Uwe McVogt <mac@uv-software.de>
2022-01-08 10:23:05 +0100

    Update examples to run with version 0.10.1
    Relates to [MACCAN-64]

--- Examples/c++/src/pcbusb_recv.cpp
+++ Examples/c++/src/pcbusb_recv.cpp
@@ -1,3 +1,12 @@
+//
+//  macOS Library for PCAN-USB Interfaces
+//  Example: receive CAN messages (Blockng read)
+//
+//  To compile type `clang++ pcbusb_recv.cpp -l PCBUSB -o pcbusb_recv'
+//  libPCBUSB.dylib is expected to be in the library search path.
+//
+//  Note: Press Ctrl+C to terminate the program.
+//
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -5,64 +14,63 @@
 #include <errno.h>
 #include <sys/time.h>
 
+#include "PCBUSB.h"
+
 #define PCAN_CHANNEL PCAN_USBBUS1
 #define PCAN_BAUDRATE PCAN_BAUD_250K
 
-#ifndef __APPLE__
-#include <asm/types.h>
-#define DWORD  __u32
-#define WORD   unsigned short
-#define BYTE   unsigned char
-#define LPSTR  char*
-#include "PCANBasic.h"
-#else
-#include "PCBUSB.h"
-#endif
-
-void sigterm(int signo)
+static volatile int running = 1;
+static void sigterm(int signo)
 {
-	/*printf("got signal %d\n", signo);*/
-	CAN_Uninitialize(PCAN_NONEBUS);
-	exit(0);
+    //printf("got signal %d\n", signo);
+    running = 0;
 }
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-	TPCANMsg message;
-	TPCANStatus status;
-	int fd;
-	fd_set fds;
+    int fdes;
+    fd_set rdfs;
 
-	if((signal(SIGTERM, sigterm) == SIG_ERR) ||
-	   (signal(SIGINT, sigterm) == SIG_ERR)) {
-		perror("Error");
-		return errno;
-	}
-	status = CAN_Initialize(PCAN_CHANNEL, PCAN_BAUDRATE, 0, 0, 0);
-	printf("Initialize CAN: 0x%x\n", status);
-	if(status != PCAN_ERROR_OK) goto leave;
+    TPCANMsg message;
+    TPCANStatus status;
 
-	status = CAN_GetValue(PCAN_CHANNEL, PCAN_RECEIVE_EVENT, &fd, sizeof(int));
-	if(status != PCAN_ERROR_OK) goto leave;
+    if ((signal(SIGTERM, sigterm) == SIG_ERR) ||
+        (signal(SIGINT, sigterm) == SIG_ERR)) {
+        perror("Error");
+        return errno;
+    }
+    status = CAN_Initialize(PCAN_CHANNEL, PCAN_BAUDRATE, 0, 0, 0);
+    printf("Initialize CAN: 0x%x\n", status);
+    if (status != PCAN_ERROR_OK) goto leave;
 
-	FD_ZERO(&fds);
-	FD_SET(fd, &fds);
+    // get a file descriptor for blocking read via `select'
+    status = CAN_GetValue(PCAN_CHANNEL, PCAN_RECEIVE_EVENT, &fdes, sizeof(int));
+    if (status != PCAN_ERROR_OK) goto leave;
 
-	while(select(fd+1, &fds, NULL, NULL, NULL) > 0) {
-		status = CAN_Read(PCAN_CHANNEL, &message, NULL);
-		if (status != PCAN_ERROR_OK) {
-			printf("Error 0x%x\n", status);
-			break;
-		}
-		printf("  - R ID:%4x LEN:%1x DATA:%02x %02x %02x %02x %02x %02x %02x %02x\n",
-				(int) message.ID, (int) message.LEN, (int) message.DATA[0],
-				(int) message.DATA[1], (int) message.DATA[2],
-				(int) message.DATA[3], (int) message.DATA[4],
-				(int) message.DATA[5], (int) message.DATA[6],
-				(int) message.DATA[7]);
-	}
+    // see `man select' for details on I/O descriptor sets
+    FD_ZERO(&rdfs);
+    FD_SET(fdes, &rdfs);
+
+    while (running) {
+        status = CAN_Read(PCAN_CHANNEL, &message, NULL);
+        if (status == PCAN_ERROR_OK) {
+            if (!(message.MSGTYPE & PCAN_MESSAGE_STATUS))
+                printf("  - R ID:%4x LEN:%1x DATA:%02x %02x %02x %02x %02x %02x %02x %02x\n",
+                    (int) message.ID, (int) message.LEN, (int) message.DATA[0],
+                    (int) message.DATA[1], (int) message.DATA[2],
+                    (int) message.DATA[3], (int) message.DATA[4],
+                    (int) message.DATA[5], (int) message.DATA[6],
+                    (int) message.DATA[7]);
+        } else if (status == PCAN_ERROR_QRCVEMPTY) {
+            // block until at least one CAN message has been received
+            if (select(fdes+1, &rdfs, NULL, NULL, NULL) < 0)
+                perror("Error");
+        } else {
+            printf("Error 0x%x\n", status);
+            break;
+        }
+    }
 leave:
-	CAN_Uninitialize(PCAN_NONEBUS);
-	return 0;
+    CAN_Uninitialize(PCAN_NONEBUS);
+    return 0;
 }
-
--- Examples/c++/src/pcbusb_send.cpp
+++ Examples/c++/src/pcbusb_send.cpp
@@ -1,44 +1,63 @@
+//
+//  macOS Library for PCAN-USB Interfaces
+//  Example: send CAN messages
+//
+//  To compile type `clang++ pcbusb_send.cpp -l PCBUSB -o pcbusb_send'
+//  libPCBUSB.dylib is expected to be in the library search path.
+//
+//  Note: Press Ctrl+C to terminate the program.
+//
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
 #include <string.h>
+
+#include "PCBUSB.h"
 
 #define PCAN_CHANNEL PCAN_USBBUS2
 #define PCAN_BAUDRATE PCAN_BAUD_250K
 
-#ifndef __APPLE__
-#include <asm/types.h>
-#define DWORD  __u32
-#define WORD   unsigned short
-#define BYTE   unsigned char
-#define LPSTR  char*
-#include "PCANBasic.h"
-#else
-#include "PCBUSB.h"
-#endif
-
-int main(int argc, char *argv[]) 
+static volatile int running = 1;
+static void sigterm(int signo)
 {
-	TPCANMsg message;
-	TPCANStatus status;
-	unsigned long n = 0;
+    //printf("got signal %d\n", signo);
+    running = 0;
+}
 
-	status = CAN_Initialize(PCAN_CHANNEL, PCAN_BAUDRATE, 0, 0, 0);
-	printf("Initialize CAN: 0x%x\n", status);
-	if(status != PCAN_ERROR_OK) goto leave;
+int main(int argc, char *argv[])
+{
+    TPCANMsg message;
+    TPCANStatus status;
+    unsigned long frames = 0;
 
-	message.ID = 0x100;
-	message.LEN = 8;
-	message.MSGTYPE = PCAN_MESSAGE_STANDARD;
-	memset(message.DATA, 0x00, message.LEN);
+    if ((signal(SIGTERM, sigterm) == SIG_ERR) ||
+        (signal(SIGINT, sigterm) == SIG_ERR)) {
+        perror("+++ Error");
+        return errno;
+    }
+    status = CAN_Initialize(PCAN_CHANNEL, PCAN_BAUDRATE, 0, 0, 0);
+    printf("Initialize CAN: 0x%x\n", status);
+    if (status != PCAN_ERROR_OK) goto leave;
 
-	for(;;) {
-	    while((status = CAN_Write(PCAN_CHANNEL, &message)) == PCAN_ERROR_OK) {
-			message.DATA[0]++;
-	        if((++n % 1000) == 0)
-		        printf("  - T Message %lu\n", n);
-	    }
-	}
+    message.ID = 0x100;
+    message.LEN = 8;
+    message.MSGTYPE = PCAN_MESSAGE_STANDARD;
+    memset(message.DATA, 0x00, message.LEN);
+
+    while (running) {
+        if ((status = CAN_Write(PCAN_CHANNEL, &message)) == PCAN_ERROR_OK) {
+            frames++;
+//            if ((frames % 2048) == 0)
+//                printf("  - T Message %lu\n", frames);
+            message.DATA[0] = (BYTE)((frames & 0x000000FFU) >> 0);
+            message.DATA[1] = (BYTE)((frames & 0x0000FF00U) >> 8);
+            message.DATA[2] = (BYTE)((frames & 0x00FF0000U) >> 16);
+            message.DATA[3] = (BYTE)((frames & 0xFF000000U) >> 24);
+        }
+    }
 leave:
-	CAN_Uninitialize(PCAN_NONEBUS);
-	return 0;
+    status = CAN_Uninitialize(PCAN_NONEBUS);
+    return 0;
 }
