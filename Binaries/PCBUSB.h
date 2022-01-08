3059f7fd23d12bc68d13c668bbd66681e853e969
Uwe McVogt <mac@uv-software.de>
2022-01-08 10:08:19 +0100

    Add artifacts of version 0.9 as of June 25, 2020
    Relates to [MACCAN-26]

--- Binaries/PCBUSB.h
+++ Binaries/PCBUSB.h
@@ -1,12 +1,12 @@
-/* -- $HeadURL: https://uv-software.net/MacCAN/PCANUSB/Library/trunk/drv/pcan_api.h $ --
+/* -- $HeadURL: https://svn.uv-software.net/MacCAN/PCANUSB/Library/trunk/drv/pcan_api.h $ --
  *
  *	project   :  CAN - Controller Area Network
  *
  *	purpose   :  PCAN Application Programming Interface
  *
- *	copyright :  (C) 2012-2019 by UV Software, Berlin
+ *	copyright :  (C) 2012-2020 by UV Software, Berlin
  *
- *	compiler  :  Apple LLVM version 10.0.0 (clang-1000.11.45.5)
+ *	compiler  :  Apple clang version 11.0.3 (clang-1103.0.32.62)
  *
  *	export    :  TPCANStatus CAN_Initialize(TPCANHandle Channel, TPCANBaudrate Btr0Btr1, TPCANType HwType, DWORD IOPort, WORD Interrupt);
  *	             TPCANStatus CAN_Uninitialize(TPCANHandle Channel);
@@ -35,7 +35,7 @@
  *	PCAN API  -  PEAK CAN Application Programming Interface
  *
  *	This Application Programming Interface (API) is a nearly compatible
- *	implementation of the PEAK PCANBasic DLL on macOS (Darwin Kernel 16.x.x).
+ *	implementation of the PEAK PCANBasic DLL on macOS (Darwin Kernel 19.x.x).
  *
  *	Supported CAN Interfaces:
  *	- PCAN-USB
@@ -43,48 +43,43 @@
  *	Up to 8 devices are supported.
  *
  *	Version of PCAN API:
- *	- Based on PEAK�s version of 06/21/2017
+ *	- Based on PEAK's version of 10/28/2019
  */
 
 #ifndef PCAN_API_H_INCLUDED
 #define PCAN_API_H_INCLUDED
 
-#ifdef __cplusplus
-extern "C" {
-#	define _DEF_ARG =0
-#else
-#	define _DEF_ARG
-#endif
-
 /*  -----------  includes  -----------------------------------------------
  */
 
+#ifdef __APPLE__
+#include <MacTypes.h>                   // To map Windows integer types
+#endif
 
 /*  -----------  defines  ------------------------------------------------
  */
 
+#ifdef __APPLE__
 #ifndef BYTE
-#define BYTE	unsigned char
+#define BYTE	UInt8
 #endif
 #ifndef WORD
-#define WORD	unsigned short
+#define WORD	UInt16
 #endif
 #ifndef DWORD
-#define DWORD	unsigned long
-#endif
-#ifndef QWORD
-#define QWORD	unsigned long long
+#define DWORD	UInt32
 #endif
 #ifndef UINT64
-#define UINT64	QWORD
+#define UINT64	UInt64
 #endif
 #ifndef LPSTR
 #define LPSTR	char*
 #endif
+#endif
 
 /* Defined and supported PCAN channels
  */
-#define PCAN_NONEBUS             0x00U //!< Undefined/default value for a PCAN bus
+#define PCAN_NONEBUS             0x00U  //!< Undefined/default value for a PCAN bus
 
 #define PCAN_USBBUS1             0x51U  //!< PCAN-USB interface, channel 1
 #define PCAN_USBBUS2             0x52U  //!< PCAN-USB interface, channel 2
@@ -94,6 +89,17 @@ extern "C" {
 #define PCAN_USBBUS6             0x56U  //!< PCAN-USB interface, channel 6
 #define PCAN_USBBUS7             0x57U  //!< PCAN-USB interface, channel 7
 #define PCAN_USBBUS8             0x58U  //!< PCAN-USB interface, channel 8
+#ifndef __APPLE__
+#define PCAN_USBBUS9             0x509U //!< PCAN-USB interface, channel 9
+#define PCAN_USBBUS10            0x50AU //!< PCAN-USB interface, channel 10
+#define PCAN_USBBUS11            0x50BU //!< PCAN-USB interface, channel 11
+#define PCAN_USBBUS12            0x50CU //!< PCAN-USB interface, channel 12
+#define PCAN_USBBUS13            0x50DU //!< PCAN-USB interface, channel 13
+#define PCAN_USBBUS14            0x50EU //!< PCAN-USB interface, channel 14
+#define PCAN_USBBUS15            0x50FU //!< PCAN-USB interface, channel 15
+#define PCAN_USBBUS16            0x510U //!< PCAN-USB interface, channel 16
+#endif
+
 
 /* PCAN error and status codes
  */
@@ -122,6 +128,7 @@ extern "C" {
 #define PCAN_ERROR_ILLPARAMVAL   0x08000U  //!< Invalid parameter value
 #define PCAN_ERROR_UNKNOWN       0x10000U  //!< Unknown error
 #define PCAN_ERROR_ILLDATA       0x20000U  //!< Invalid data, function, or action
+#define PCAN_ERROR_ILLMODE       0x80000U  //!< Driver object state is wrong for the attempted operation
 #define PCAN_ERROR_CAUTION       0x2000000U  //!< An operation was successfully carried out, however, irregularities were registered
 #define PCAN_ERROR_INITIALIZE    0x4000000U  //!< Channel is not initialized [Value was changed from 0x40000 to 0x4000000]
 #define PCAN_ERROR_ILLOPERATION  0x8000000U  //!< Invalid operation [Value was changed from 0x80000 to 0x8000000]
@@ -129,7 +136,7 @@ extern "C" {
 /* PCAN devices
  */
 #define PCAN_NONE                0x00U  //!< Undefined, unknown or not selected PCAN device value
-#define PCAN_PEAKCAN             0x01U  //!< PCAN Non-Plug&Play devices. NOT USED WITHIN PCAN-Basic API
+#define PCAN_PEAKCAN             0x01U  //!< PCAN Non-PnP devices. NOT USED WITHIN PCAN-Basic API
 #define PCAN_ISA                 0x02U  //!< PCAN-ISA, PCAN-PC/104, and PCAN-PC/104-Plus
 #define PCAN_DNG                 0x03U  //!< PCAN-Dongle
 #define PCAN_PCI                 0x04U  //!< PCAN-PCI, PCAN-cPCI, PCAN-miniPCI, and PCAN-PCI Express
@@ -140,8 +147,8 @@ extern "C" {
 
 /* PCAN parameters
  */
-#define PCAN_DEVICE_NUMBER       0x01U  //!< PCAN-USB device number parameter
-#define PCAN_5VOLTS_POWER        0x02U  //!< PCAN-PC Card 5-Volt power parameter
+#define PCAN_DEVICE_ID           0x01U  //!< Device identifier parameter
+#define PCAN_5VOLTS_POWER        0x02U  //!< 5-Volt power parameter
 #define PCAN_RECEIVE_EVENT       0x03U  //!< PCAN receive event handler parameter
 #define PCAN_MESSAGE_FILTER      0x04U  //!< PCAN message filter parameter
 #define PCAN_API_VERSION         0x05U  //!< PCAN-Basic API version parameter
@@ -175,6 +182,14 @@ extern "C" {
 #define PCAN_INTERFRAME_DELAY    0x21U  //!< Delay, in microseconds, between sending frames
 #define PCAN_ACCEPTANCE_FILTER_11BIT 0x22U  //!< Filter over code and mask patterns for 11-Bit messages
 #define PCAN_ACCEPTANCE_FILTER_29BIT 0x23U  //!< Filter over code and mask patterns for 29-Bit messages
+#define PCAN_IO_DIGITAL_CONFIGURATION 0x24U //!< Output mode of 32 digital I/O pin of a PCAN-USB Chip. 1: Output-Active 0 : Output Inactive
+#define PCAN_IO_DIGITAL_VALUE         0x25U //!< Value assigned to a 32 digital I/O pins of a PCAN-USB Chip
+#define PCAN_IO_DIGITAL_SET           0x26U //!< Value assigned to a 32 digital I/O pins of a PCAN-USB Chip - Multiple digital I/O pins to 1 = High
+#define PCAN_IO_DIGITAL_CLEAR         0x27U //!< Clear multiple digital I/O pins to 0
+#define PCAN_IO_ANALOG_VALUE          0x28U //!< Get value of a single analog input pin
+#define PCAN_FIRMWARE_VERSION         0x29U //!< Get the version of the firmware used by the device associated with a PCAN-Channel
+#define PCAN_ATTACHED_CHANNELS_COUNT  0x2AU //!< Get the amount of PCAN channels attached to a system
+#define PCAN_ATTACHED_CHANNELS        0x2BU //!< Get information about PCAN channels attached to a system
 #define PCAN_EXT_BTR0BTR1        0x80U  //!< UVS: bit-timing register
 #define PCAN_EXT_TX_COUNTER      0x81U  //!< UVS: number of transmitted frames
 #define PCAN_EXT_RX_COUNTER      0x82U  //!< UVS: number of received frames
@@ -185,6 +200,10 @@ extern "C" {
 #define PCAN_EXT_RECEIVE_CALLBACK 0x87U //!< UVS: callback function called on the reception of an URB
 #define PCAN_EXT_LOG_USB         0x8FU  //!< UVS: Log USB communication (URB buffer <==> CAN messages)
 
+/* DEPRECATED parameters
+ */
+#define PCAN_DEVICE_NUMBER       PCAN_DEVICE_ID  //!< Deprecated parameter. Use PCAN_DEVICE_ID instead 
+
 /* PCAN parameter values
  */
 #define PCAN_PARAMETER_OFF       0x00U  //!< The PCAN parameter is not set (inactive)
@@ -193,7 +212,7 @@ extern "C" {
 #define PCAN_FILTER_OPEN         0x01U  //!< The PCAN filter is fully opened. All messages will be received
 #define PCAN_FILTER_CUSTOM       0x02U  //!< The PCAN filter is custom configured. Only registered messages will be received
 #define PCAN_CHANNEL_UNAVAILABLE 0x00U  //!< The PCAN-Channel handle is illegal, or its associated hardware is not available
-#define PCAN_CHANNEL_AVAILABLE   0x01U  //!< The PCAN-Channel handle is available to be connected (Plug&Play Hardware: it means furthermore that the hardware is plugged-in)
+#define PCAN_CHANNEL_AVAILABLE   0x01U  //!< The PCAN-Channel handle is available to be connected (PnP Hardware: it means furthermore that the hardware is plugged-in)
 #define PCAN_CHANNEL_OCCUPIED    0x02U  //!< The PCAN-Channel handle is valid, and is already being used
 #define PCAN_CHANNEL_PCANVIEW    (PCAN_CHANNEL_AVAILABLE |  PCAN_CHANNEL_OCCUPIED) //!< The PCAN-Channel handle is already being used by a PCAN-View application, but is available to connect
 
@@ -213,10 +232,16 @@ extern "C" {
 
 #define FEATURE_FD_CAPABLE       0x01U  //!< Device supports flexible data-rate (CAN-FD)
 #define FEATURE_DELAY_CAPABLE    0x02U  //!< Device supports a delay between sending frames (FPGA based USB devices)
+#define FEATURE_IO_CAPABLE       0x04U  //!< Device supports I/O functionality for electronic circuits (USB-Chip devices)
 
 #define SERVICE_STATUS_STOPPED   0x01U  //!< The service is not running
 #define SERVICE_STATUS_RUNNING   0x04U  //!< The service is running
 
+/* Other constants
+ */
+#define MAX_LENGTH_HARDWARE_NAME   33   //!< Maximum length of the name of a device: 32 characters + terminator
+#define MAX_LENGTH_VERSION_STRING  18   //!< Maximum length of a version string: 17 characters + terminator
+								      
 /* PCAN message types
  */
 #define PCAN_MESSAGE_STANDARD    0x00U  //!< The PCAN message is a CAN Standard Frame (11-bit identifier)
@@ -225,6 +250,7 @@ extern "C" {
 #define PCAN_MESSAGE_FD          0x04U  //!< The PCAN message represents a FD frame in terms of CiA Specs
 #define PCAN_MESSAGE_BRS         0x08U  //!< The PCAN message represents a FD bit rate switch (CAN data at a higher bit rate)
 #define PCAN_MESSAGE_ESI         0x10U  //!< The PCAN message represents a FD error state indicator(CAN FD transmitter was error active)
+#define PCAN_MESSAGE_ERRFRAME    0x40U  //!< The PCAN message represents an error frame
 #define PCAN_MESSAGE_STATUS      0x80U  //!< The PCAN message represents a PCAN status message
 
 /* Frame Type / Initialization Mode
@@ -276,8 +302,8 @@ extern "C" {
 /*  -----------  types  --------------------------------------------------
  */
 
-#define TPCANHandle              WORD  //!< PCAN hardware channel handle (ATTENTION: changed from BYTE to WORD)
-#define TPCANStatus              DWORD //!< PCAN status/error code
+#define TPCANHandle              WORD  //!< PCAN hardware channel handle
+#define TPCANStatus              DWORD //!< PCAN status/error code (ATTENTION: changed from 64-bit to 32-bit)
 #define TPCANParameter           BYTE  //!< PCAN parameter to be read or set
 #define TPCANDevice              BYTE  //!< PCAN device
 #define TPCANMessageType         BYTE  //!< The type of a PCAN message
@@ -291,27 +317,27 @@ extern "C" {
  */
 typedef struct tagTPCANMsg
 {
-    DWORD             ID;      //!< 11/29-bit message identifier
+    DWORD             ID;      //!< 11/29-bit message identifier (ATTENTION: changed from 64-bit to 32-bit)
     TPCANMessageType  MSGTYPE; //!< Type of the message
     BYTE              LEN;     //!< Data Length Code of the message (0..8)
     BYTE              DATA[8]; //!< Data of the message (DATA[0]..DATA[7])
 } TPCANMsg;
 
 /** Timestamp of a received PCAN message
- *  Total Microseconds = micros + 1000 * millis + 0xFFFFFFFF * 1000 * millis_overflow
+ *  Total Microseconds = micros + 1000 * millis + 0x100000000 * 1000 * millis_overflow
  */
 typedef struct tagTPCANTimestamp
 {
-    DWORD  millis;             //!< Base-value: milliseconds: 0.. 2^32-1
+    DWORD  millis;             //!< Base-value: milliseconds: 0.. 2^32-1 (ATTENTION: changed from 64-bit to 32-bit)
     WORD   millis_overflow;    //!< Roll-arounds of millis
     WORD   micros;             //!< Microseconds: 0..999
 } TPCANTimestamp;
 
 /** PCAN message from a FD capable hardware
  */
-typedef struct tagPCANMsgFD
+typedef struct tagTPCANMsgFD
 {
-    DWORD             ID;      //!< 11/29-bit message identifier
+    DWORD             ID;      //!< 11/29-bit message identifier (ATTENTION: changed to 32-bit)
     TPCANMessageType  MSGTYPE; //!< Type of the message
     BYTE              DLC;     //!< Data Length Code of the message (0..15)
     BYTE              DATA[64];//!< Data of the message (DATA[0]..DATA[63])
@@ -324,6 +350,13 @@ typedef struct tagPCANMsgFD
 
 /*  -----------  prototypes  ---------------------------------------------
  */
+
+#ifdef __cplusplus
+extern "C" {
+#define _DEF_ARG =0
+#else
+#define _DEF_ARG
+#endif
 
 /** @brief       Initializes a PCAN Channel.
  *
